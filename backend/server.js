b81dd93d5c7a72d445a7b3116e2072fa9f9cc80a
MattiaPetru <mattiapetru93@gmail.com>
2024-07-19 20:23:45 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express"; // Framework web per Node.js
 import mongoose from "mongoose"; // ODM per MongoDB
 import dotenv from "dotenv"; // Per caricare variabili d'ambiente da file .env
-import cors from "cors"; // Middleware per gestire CORS (Cross-Origin Resource Sharing)
+import cors from "cors"; // Middleware per gestire CORS
 import listEndpoints from "express-list-endpoints"; // Utility per elencare gli endpoints dell'app
 import authorRoutes from "./routes/authorRoutes.js"; // Rotte per gli autori
 import blogPostRoutes from "./routes/blogPostRoutes.js"; // Rotte per i blog post
 import authRoutes from "./routes/authRoutes.js"; // Rotte per l'autenticazione
-import session from "express-session"; // NEW! Importiamo session
-import passport from "./config/passportConfig.js"; // NEW! importiamo passport
+import session from "express-session";
+import passport from "./config/passportConfig.js";
 
 // MIDDLEWARE Importazione dei middleware per la gestione degli errori
 import {
@@ -48,25 +48,19 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Abilita CORS per tutte le rotte
 app.use(express.json()); // Parsing del corpo delle richieste in formato JSON
 
-// NEW! Configurazione della sessione
+//Configurazione della sessione
 app.use(
   session({
     // Il 'secret' è usato per firmare il cookie di sessione
     // È importante che sia una stringa lunga, unica e segreta
     secret: process.env.SESSION_SECRET,
-
-    // 'resave: false' dice al gestore delle sessioni di non
-    // salvare la sessione se non è stata modificata
+    // 'resave: false' dice al gestore delle sessioni di non salvare la sessione se non è stata modificata
     resave: false,
-
-    // 'saveUninitialized: false' dice al gestore delle sessioni di non
-    // creare una sessione finché non memorizziamo qualcosa
-    // Aiuta a implementare le "login sessions" e riduce l'uso del server di memorizzazione
     saveUninitialized: false,
   })
 );
 
-// NEW! Inizializzazione di Passport
+//Inizializzazione di Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -77,7 +71,7 @@ mongoose
   .catch((err) => console.error("Errore di connessione MongoDB:", err));
 
 // Definizione delle rotte principali
-app.use("/api/auth", authRoutes); // NEW! Rotte per l'autenticazione
+app.use("/api/auth", authRoutes); // Rotte per l'autenticazione
 app.use("/api/authors", authorRoutes); // Rotte per gli autori
 app.use("/api/blogPosts", blogPostRoutes); // Rotte per i blog post
 
