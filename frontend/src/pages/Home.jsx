23f9a56b146edc9817af523ddccd0d365ac3820e
MattiaPetru <mattiapetru93@gmail.com>
2024-07-22 19:38:44 +0200

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -52,22 +52,32 @@ export default function Home({ isLoggedIn, setIsLoggedIn }) {
   return (
     <div className="container">
       <h1>Lista dei Post</h1>
-      <SearchBar onSearch={handleSearch} />
+      <div className="search-container">
+        <SearchBar onSearch={handleSearch} />
+      </div>
       <div className="post-grid">
         {filteredPosts.map((post) => (
-          <Link to={`/post/${post._id}`} key={post._id} className="post-card">
-            <img src={post.cover} alt={post.title} className="post-image" />
-            <div className="post-content">
-              <h2>{post.title}</h2>
-              <p>Autore: {post.author}</p>
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" onClick={() => handleDeletePost(post._id)}>
-             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          <div key={post._id} className="post-card">
+            <Link to={`/post/${post._id}`} className="post-link">
+              <img src={post.cover} alt={post.title} className="post-image" />
+              <div className="post-content">
+                <h2>{post.title}</h2>
+                <p>Autore: {post.author}</p>
+              </div>
+            </Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-trash delete-icon"
+              viewBox="0 0 16 16"
+              onClick={() => handleDeletePost(post._id)}
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
             </svg>
-            </div>
-            
-          </Link>
-          
+          </div>
         ))}
       </div>
     </div>
